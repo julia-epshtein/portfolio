7030b6227de5ec1660c5c3ef2cf562714cba4bbe
Julia Epshtein <juliaepshtein@Julias-MBP.lan>
2023-07-09 18:07:29 -0700

    Styled NavBar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
-    <h1 className="text-4xl font-bold font-signature">Hello World</h1>  
+    <div>
+      <NavBar />
+    </div>
   );
 }
 
