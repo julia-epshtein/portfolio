b804a93eb6c2892dd167733d40fe5bd4b1e8bf84
unknown <jepshtein@umass.edu>
2023-08-11 22:45:13 -0700

    Contact

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import NavBar from "./components/NavBar";
 import About from "./components/About"
 import SocialLinks from "./components/SocialLinks";
 import Portfolio from "./components/Portfolio";
+import Experience from "./components/Experience";
+import Personal from "./components/Personal";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +14,8 @@ function App() {
       <Home />
       <About />
       <Portfolio />
+      <Experience />
+      <Contact />
       <SocialLinks />
     </div>
   );
