f5830256b901b60980ef759523a2b549b22f76a6
unknown <jepshtein@umass.edu>
2023-08-23 13:53:12 -0700

    edited appearance of contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,25 +3,52 @@ import HeaderStyle from '../utilities/HeaderStyle';
 import { motion } from 'framer-motion';
 
 const Contact = () => {
-
     return (
         <div name="contact" className="w-full bg-gradient-to-b from-black via-black to-deep-blue text-white md:h-screen">
-            <div className="max-w-screen-lg w-full p-4 mx-auto flex flex-col justify-center">
+            <div className="max-w-screen-lg w-full p-8 mx-auto flex flex-col justify-center">
                 <div className="pb-8">
                     <HeaderStyle headerText="contact" />
-                    <p className="py-6">Submit the form below to get in touch with me</p>
+                    <p className="pt-8 text-lg">Submit the form below to get in touch with me!</p>
                 </div>
-                <div className="flex justify-center items-center">
-                    <form action="https://getform.io/f/8d0cd474-8d38-4f0e-a3ca-86df40050aa5" method="POST" className="flex flex-col w-full md:w-1/2">
-                        <input type="text" name="name" placeholder="Enter your name" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
-                        <input type="text" name="email" placeholder="Enter your email" className="my-4 p-2 bg-transparent border-2 rounded-md text-white focus:outline-none" />
-                        <textarea name="message" placeholder="Enter your message" className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"></textarea>
-                        <button className="text-white bg-gradient-to-b from-violet-400 to-purple-700 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">Send</button>
-                    </form>
+                <div className="flex justify-start items-center">
+                    <div className="bg-deep-blue rounded-3xl p-8 w-full md:w-1/2">
+                        <div className="mb-6">
+                            <label className="block text-white mb-2 text-lg" htmlFor="name">Your Name</label>
+                            <input
+                                type="text"
+                                name="name"
+                                id="name"
+                                placeholder="Enter your name"
+                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-white mb-2 text-lg" htmlFor="email">Your Email</label>
+                            <input
+                                type="text"
+                                name="email"
+                                id="email"
+                                placeholder="Enter your email"
+                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-white mb-2 text-lg" htmlFor="message">Your Message</label>
+                            <textarea
+                                name="message"
+                                id="message"
+                                placeholder="Enter your message"
+                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                            ></textarea>
+                        </div>
+                        <button className="text-white bg-purple-700 px-8 py-4 mx-auto rounded-md hover:scale-110 duration-300">
+                            Send
+                        </button>
+                    </div>
                 </div>
             </div>
-        </div >
-    )
+        </div>
+    );
 }
 
 export default Contact;
