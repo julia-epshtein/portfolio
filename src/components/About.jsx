08c134ffa72372630163ff04499bca44867fa040
Julia Epshtein <jepshtein03@gmail.com>
2023-08-31 14:35:16 -0400

    small spelling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
                     >
                         <HeaderStyle headerText="About Me" />
                         <Paragraph>
-                            I am an honors CS & Data Science student at UMass. I have experience building React Apps, developing coursework, and teaching computer science and math, and I'm interested in frontend development, machine learning, and exploring the intersection between computer science and education. At UC San Diego, I'm researching the reliability of spreadsheet software at UC San Diego by developing and piloting a custom Jupyter Notebooks Widget using Python, React, and the Javascript AG-Grid framework. I am also a course assistant for CS 240: Reasoning Under Uncertainty and a math tutor at the Russian School of Math.
+                            I am an honors CS & Data Science student at UMass. I have experience building React Apps, developing coursework, and teaching computer science and math, and I'm interested in frontend development, machine learning, and exploring the intersection between computer science and education. At UC San Diego, I'm researching the reliability of spreadsheet software by developing and piloting a custom Jupyter Notebooks Widget using Python, React, and the Javascript AG-Grid framework. I am also a course assistant for CS 240: Reasoning Under Uncertainty and a math tutor at the Russian School of Math.
                         </Paragraph>
                     </motion.div>
                 </div>
