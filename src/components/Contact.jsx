16b62c3ff2b3bd96fdc59782e5d0b4539bdbb07b
Julia Epshtein <jepshtein03@gmail.com>
2024-01-18 22:47:40 -0500

    updated contact form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,48 +1,48 @@
 import React from 'react';
 import HeaderStyle from '../utilities/HeaderStyle';
-import { motion } from 'framer-motion';
+import { HiOutlineMail } from 'react-icons/hi';
 
 const Contact = () => {
     return (
-        <div name="contact" className="w-full h-screen bg-gradient-to-b from-black via-black to-deep-blue text-white pt-20">
+        <div name="contact" className="w-full h-screen bg-gradient-to-br from-violet-400 to-sky-300 text-black pt-20">
             <div className="max-w-screen-lg w-full mx-auto flex flex-col justify-center">
                 <div className="pb-8">
-                    <HeaderStyle headerText="Contact" />
-                    <p className="pt-8 pb-12 text-lg">Submit the form below to get in touch with me!</p>
+                    <div className = "pl-8"><HeaderStyle headerText="Contact" /></div>
+                    <p className="pt-8 pl-8 font-source font-bold text-black text-2xl">Submit the form below to get in touch with me!</p>
                 </div>
-                <div className="flex justify-start items-center">
-                    <div className="bg-deep-blue rounded-3xl p-8 w-full md:w-3/4">
+                <div className="flex justify-start items-center p-8">
+                    <div className="bg-neutral-800 rounded-3xl p-8 w-full md:w-3/4">
                         <div className="mb-6">
-                            <label className="block text-white mb-2 text-lg" htmlFor="name">Your Name</label>
+                            <label className="block text-white mb-2 font-paragraph" htmlFor="name">Your Name</label>
                             <input
                                 type="text"
                                 name="name"
                                 id="name"
-                                placeholder="Enter your name"
-                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                                className="p-4 bg-neutral-900 rounded-md text-white w-full focus:outline-none text-lg"
                             />
                         </div>
                         <div className="mb-6">
-                            <label className="block text-white mb-2 text-lg" htmlFor="email">Your Email</label>
+                            <label className="block text-white mb-2 font-paragraph" htmlFor="email">Your Email</label>
                             <input
                                 type="text"
                                 name="email"
                                 id="email"
-                                placeholder="Enter your email"
-                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                                className="p-4 bg-neutral-900 rounded-md text-white w-full focus:outline-none text-lg"
                             />
                         </div>
                         <div className="mb-6">
-                            <label className="block text-white mb-2 text-lg" htmlFor="message">Your Message</label>
+                            <label className="block text-white mb-2 font-paragraph" htmlFor="message">Your Message</label>
                             <textarea
                                 name="message"
                                 id="message"
-                                placeholder="Enter your message"
-                                className="p-4 bg-transparent border-2 rounded-md text-white w-full focus:outline-none text-lg"
+                                className="p-4 bg-neutral-900 rounded-md text-white w-full focus:outline-none text-lg"
                             ></textarea>
                         </div>
-                        <button className="text-white bg-purple-700 px-8 py-4 mx-auto rounded-md hover:scale-110 duration-300">
-                            Send
+                        <button className="text-white bg-black font-source text-2xl px-8 py-4 mx-auto rounded-xl hover:scale-110 duration-300">
+                            <span>
+                                Send {" "}
+                                <HiOutlineMail size={30} className="inline-block ml-2" />
+                            </span>
                         </button>
                     </div>
                 </div>
